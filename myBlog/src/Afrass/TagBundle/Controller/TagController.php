02835421548aa7a7294546e2b6c8d388205779f2
Ilias Afrass <ilias.afrass@etudiant.univ-lille1.fr>
2019-02-26 13:16:11 +0100

    enhance Tag's view and add auto completition
    add typehead and bootstrap-tagsinput

---
+++ myBlog/src/Afrass/TagBundle/Controller/TagController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Afrass\TagBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class TagController extends Controller
+{
+}
