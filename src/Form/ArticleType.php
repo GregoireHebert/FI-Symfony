d51f5c4c3d7ec0f2eec4019eb9dd8747650a1d9f
david <david.guillon@outlook.fr>
2019-02-27 23:16:24 +0100

    feat(Article)
    Add Article CRUD
    Add Article Repository
    Add Article form
    add Article templates
    Add Article Routes
    Add some config  files

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('subtitle')
+            ->add('corpus')
+            ->add('createdAt', DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
