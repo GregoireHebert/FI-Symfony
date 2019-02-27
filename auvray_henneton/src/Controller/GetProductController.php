5c40f63278806ab4fcf0bd84b96b635c67263f02
lhenneto <laury.henneton@etudiant.univ-lille1.fr>
2019-02-27 22:46:42 +0100

    page d'article

---
+++ auvray_henneton/src/Controller/GetProductController.php
@@ -0,0 +1,27 @@
+<?php
+declare (strict_types = 1);
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use App\Entity\Product;
+use App\Form\ArticleFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class GetProductController extends AbstractController{
+    public function __invoke(Request $request,EntityManagerInterface $em){
+        $repository = $em->getRepository(Product::class);
+        $prostId = $request->get('id');
+        $post = $repository->find($prostId);
+        
+        return $this->render('getPost.html.twig', ['post' => $post]);
+
+    }
+
+  
+}
+
+?>
