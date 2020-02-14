90b3189221c665590e0794524df1555bb6358eb8
rgiaquinto <raphaelgiaquinto@gmail.com>
2020-02-14 10:02:43 +0100

    feat(entity): Add Category.php in the Entity directory

---
+++ src/Entity/Category.php
@@ -0,0 +1,30 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+/**
+ * Class wich represents a product category
+ * 
+ */
+final class Category{
+
+
+    private $id;
+
+    private $name;
+
+    public function __construct(){
+        $this->id = \App\ORM\Util\UUID::v4();
+    }
+
+    public function getId() :string {
+        return $this->id;
+    }
+
+    public function getName() :string {
+        return $this->name;
+    }
+}
+?>
