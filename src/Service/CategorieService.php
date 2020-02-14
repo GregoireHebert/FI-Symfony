49125e344fe66f9072a5e37546f26ec2dabae71f
Gallardo Marie <gallardo@b11p7.fil.univ-lille1.fr>
2020-02-14 09:51:38 +0100

    [FEAT] Ajout du Service de Categorie

--- src/Service/CategorieService.php
+++ src/Service/CategorieService.php
@@ -3,3 +3,29 @@
 declare(strict_types=1);
 
 namespace App\Service;
+
+final class CategorieService {
+
+    protected $em;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+
+    public function listerCategories() : array{
+        return $this->em->findAll("categorie");
+    }
+
+    public function listerProduitsParCategorie(int $ref) : array {
+        $result = [];
+        $allProduits = $this->em->findAll("produit");
+        foreach($allProduits as $produit) {
+            if($produit->$ref == $ref) {
+                array_push($result,$produit);
+            }
+        }
+        return $result;
+
+    }
+}
