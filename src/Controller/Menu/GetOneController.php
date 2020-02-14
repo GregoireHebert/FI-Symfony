d831464c0b0ddae4b6a3bb3119d9a877adaf19c7
Massiaux Alexis <massiaux@b11p13.fil.univ-lille1.fr>
2020-02-14 10:39:54 +0100

    feat: menu controller

---
+++ src/Controller/Menu/GetOneController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Menu;
+
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use App\Exception\NotFoundHttpException;
+use App\Entity\Menu;
+
+class GetOneController
+{
+    public function __invoke(Request $request, Container $container)
+    {
+        if (null === $menu = $this->entityManager->find(Menu::class, $request->get('id'))) {
+            throw new NotFoundHttpException();
+        }
+
+        return new JsonResponse([
+            'Data' => $menu
+        ]);
+    }
+}
