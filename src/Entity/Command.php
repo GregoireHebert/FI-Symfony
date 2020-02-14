8057e53372d885e55542066d5af043964c24224c
Antoine Perrot <antoine@radioking.com>
2020-02-14 12:14:20 +0100

    Fix git stuff

--- src/Entity/Command.php
+++ src/Entity/Command.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-class Command 
+class Command
 {
     private $id;
     private $products;
@@ -11,7 +11,7 @@ class Command
 
     /**
      * constructs the command entity
-     * 
+     *
      * @param products: the list of products
      * @param menus: the list of menus
      */
@@ -25,20 +25,20 @@ class Command
 
     /**
      * gets the command id
-     * 
+     *
      * @return the command id
      */
-    public function getId() : string 
+    public function getId() : string
     {
         return $this->id;
     }
 
     /**
      * gets the total price of the command
-     * 
+     *
      * @return the total price
      */
-    public function getTotal() :  float 
+    public function getTotal() :  float
     {
         return $this->total;
     }
@@ -55,13 +55,11 @@ class Command
     public function getMenus() : array
     {
         return $this->menus;
-<<<<<<< HEAD
-=======
     }
 
     /**
      * adds product in products list
-     * 
+     *
      * @param prodcuct : the product to add
      */
     public function addProduct(Product $product)
@@ -69,32 +67,14 @@ class Command
         $this->products[$product->getName()] += 1;
 
         $this->total += $product->getPrice();
->>>>>>> 0083357dac2b32721a6d893fa2732702943c7957
     }
 
     /**
      * adds product in products list
-     * 
-     * @param prodcuct : the product to add
-<<<<<<< HEAD
-     */
-    public function addProduct(Product $product)
-    {
-        $this->products[$product->getName()] += 1;
-
-        $this->total += $product->getPrice();
-    }
-
-    /**
-     * adds product in products list
-     * 
+     *
      * @param prodcuct : the product to add
      * @param qty : the quantity of the ptoduct
      */
-=======
-     * @param qty : the quantity of the ptoduct
-     */
->>>>>>> 0083357dac2b32721a6d893fa2732702943c7957
     public function addProductQuantity(Product $product, int $qty)
     {
         $this->products[$product->getName()] += $qty;
@@ -104,7 +84,7 @@ class Command
 
     /**
      * adds menu in the menus list
-     * 
+     *
      * @param menu : the menu list
      */
     public function addMenu(Menu $menu)
@@ -116,31 +96,15 @@ class Command
 
     /**
      * adds menu in the menus list
-     * 
+     *
      * @param menu : the menu list
      * @param qty : the quantity of the menu
      */
     public function addMenuQuantity(Menu $menu, int $qty){
-        
+
         $this->menus[$menu->getName()]+= $qty;
 
         $this->total += ($menu->getPrice() * $qty);
-<<<<<<< HEAD
     }
 
-    /**
-     * return command in json format
-     */
-    public function toJson(): array
-    {
-        return [
-            'id' => $this->$id,
-            'price' => $this->$price,
-            'products' => $this->products,
-            'menus' => $this->menus
-        ];
-=======
->>>>>>> 0083357dac2b32721a6d893fa2732702943c7957
-    }
-    
 }
