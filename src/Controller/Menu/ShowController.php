d163caa08c29bcd7970633821a626425f0051444
Tao <tao@tymate.com>
2020-02-14 12:00:55 +0100

    add show controller for menus

---
+++ src/Controller/Menu/ShowController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Menu;
+
+use App\Entity\Menu;
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ShowController
+{
+    public function __invoke(Request $request, Container $container, int $id)
+    {
+        $entityManager = $container->get('entity.manager');
+        $menu = $entityManager->find(Menu::class, $id);
+
+        return $this->show($menu);
+    }
+
+    public function show(Menu $menu)
+    {
+        return new JsonResponse(['data' => $menu->toJson()]);
+    }
+}
